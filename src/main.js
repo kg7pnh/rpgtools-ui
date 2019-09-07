6906a12bc84c41d1f6e47bcabb869b620b7ce672
Michael Bayha <michael.bayha@gmail.com>
2019-09-07 08:30:23 -0700

    many changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import BootstrapVue from "bootstrap-vue";
 
-Vue.config.productionTip = false
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+import { store } from "./Datastore";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
